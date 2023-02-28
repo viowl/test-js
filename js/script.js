4deec509fb8b57546c81aac0992dcdefda2d7ff4
viowl <sovaworkbox@gmail.com>
2023-02-28 23:38:20 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-const age = 18;
-const totalPrice = 36.7;
-const massage = "Dobro pozhalovat !";
-const isOpen = true;
-const shouldConfirm = falshe;
+const pricePerItem = 2000;
 
-console.log(totalPrice);
+const productName = "Dior";
+
+console.log(productName);
+
+console.log(pricePerItem);
