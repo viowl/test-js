50eaf37f5c1faea45ada0557c246dd78fa2225f5
viowl <sovaworkbox@gmail.com>
2023-03-17 03:58:16 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
-function calculateEngravingPrice(message, pricePerWord) {
-    
-    const words = message.split(" ");
-    const wordsCount = words.length;
-    const totalPrice = wordsCount * pricePerWord;
-    return totalPrice;
-    >=
-    
-   
-    }
-   
- 
+const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+for (let i = 0; i < fruits.length; i += 1) { // Change this line
+  const fruit = fruits[i]; // Change this line
+  console.log(fruit);
+}
