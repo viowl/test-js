ad969e35adde0bfb3cff6c2f0fc13b6052141bee
viowl <sovaworkbox@gmail.com>
2023-03-17 07:21:40 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-const fruits = ['apple', 'plum', 'pear', 'orange'];
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
 
-for (let i = 0; i < fruits.length; i += 1) { // Change this line
-  const fruit = fruits[i]; // Change this line
-  console.log(fruit);
+// for (let i = 0; i < fruits.length; i += 1) { // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
+
+const start = 6;
+const end = 27;
+let number;
+
+for (let i = start; i < end; i += 1) {
+  if (i % 5 === 0) {
+    number = i;
+  break;
+  }
 }
+console.log(number)
